5b8a443aca40ae44133dbe4bf9e214e726acb6a3
mkh <m72.m27@gmail.com>
2018-04-07 02:04:26 +0430

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,10 @@ socket.on('connect', function (){
 });
 
 socket.on('newMessage', (message) =>{
-	console.log("newMessage" , message);
+	var formattedTime = moment().format("hh:mm a");
 
 	var li = jQuery("<li></li>");
-	li.text(`${message.from}: ${message.text}`);
+	li.text(`${message.from} ${formattedTime}: ${message.text}`);
 	jQuery("#messages").append(li);
 });
 
@@ -21,7 +21,8 @@ socket.on('newLocationMessage', (message) =>{
 
 	var li = jQuery("<li></li>");
 	var a = jQuery("<a target='_blank'>My current location</a>");
-	li.text(`${message.from}:`);
+	var formattedTime = moment().format("hh:mm a");
+	li.text(`${message.from} ${formattedTime}:`);
 	a.attr("href", message.url);
 	li.append(a);
 	jQuery("#messages").append(li);
@@ -30,11 +31,13 @@ socket.on('newLocationMessage', (message) =>{
 jQuery("#message-form").on("submit", function(e){
 	e.preventDefault();
 
+	var messageBox = jQuery("[name=message");
+
 	socket.emit("createMessage",{
 		from: "User",
-		text: jQuery("[name=message").val()
-	}, function(data){
-		console.log("Got it" ,data);
+		text: messageBox.val()
+	}, function(){
+		messageBox.val("");
 	});
 
 });
@@ -47,12 +50,17 @@ locationButton.on("click", function(){
 		return alert("Geolocation not supported by your browser.");
 	}
 
+	locationButton.attr("disabled", "disabled").text("Sending location...");
+
 	navigator.geolocation.getCurrentPosition(function (position){
+
+		locationButton.removeAttr("disabled").text("Send location");
 		socket.emit("createLocationMessage", {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		})
 	}, function(){
+		locationButton.removeAttr("disabled").text("Send location");
 		alert("Unable to fetch location.")
 	});
 });
